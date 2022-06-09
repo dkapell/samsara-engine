2312cfff22bb50a4cb750961cee7d5601eee36ae
dkapell <david@rigitech.com>
2022-06-09 17:32:43 -0400

    update almost all models to support multi-game

--- models/link.js
+++ models/link.js
@@ -1,88 +1,18 @@
 'use strict';
 const async = require('async');
 const _ = require('underscore');
-const database = require('../lib/database');
 const validator = require('validator');
-const cache = require('../lib/cache');
 
-const models = {
-};
+const Model = require('../lib/Model');
 
-const tableFields = ['game_id', 'name', 'description', 'url', 'gm', 'active'];
+const tableFields = ['id', 'game_id', 'name', 'description', 'url', 'gm', 'active'];
 
+const Link = new Model('links', tableFields, {
+    order: ['name'],
+    validator: validate
+});
 
-exports.get = async function(id){
-    let link = await cache.check('link', id);
-    if (link) { return link; }
-    const query = 'select * from links where id = $1';
-    const result = await database.query(query, [id]);
-    if (result.rows.length){
-        link = result.rows[0];
-        await cache.store('link',id, link);
-        return link;
-    }
-    return;
-};
-
-exports.list = async function(){
-    const query = 'select * from links order by name';
-    const result = await database.query(query);
-    return result.rows;
-};
-
-exports.create = async function(data){
-    if (! validate(data)){
-        throw new Error('Invalid Data');
-    }
-    const queryFields = [];
-    const queryData = [];
-    const queryValues = [];
-    for (const field of tableFields){
-        if (_.has(data, field)){
-            queryFields.push(field);
-            queryValues.push('$' + queryFields.length);
-            queryData.push(data[field]);
-        }
-    }
-
-    let query = 'insert into links (';
-    query += queryFields.join (', ');
-    query += ') values (';
-    query += queryValues.join (', ');
-    query += ') returning id';
-
-    const result = await database.query(query, queryData);
-    return result.rows[0].id;
-};
-
-exports.update = async function(id, data){
-    if (! validate(data)){
-        throw new Error('Invalid Data');
-    }
-    const queryUpdates = [];
-    const queryData = [id];
-    for (const field of tableFields){
-        if (_.has(data, field)){
-            queryUpdates.push(field + ' = $' + (queryUpdates.length+2));
-            queryData.push(data[field]);
-        }
-    }
-
-    let query = 'update links set ';
-    query += queryUpdates.join(', ');
-    query += ' where id = $1';
-
-    await database.query(query, queryData);
-    await cache.invalidate('link', id);
-};
-
-exports.delete = async  function(id){
-    const query = 'delete from links where id = $1';
-    await database.query(query, [id]);
-    await cache.invalidate('link', id);
-};
-
-
+module.exports = Link;
 
 function validate(data){
     if (! validator.isLength(data.name, 2, 80)){
--- models/read_message.js
+++ models/read_message.js
@@ -2,85 +2,36 @@
 const async = require('async');
 const _ = require('underscore');
 const database = require('../lib/database');
-const validator = require('validator');
 
-const models = {
-};
+const Model = require('../lib/Model');
 
-const tableFields = ['user_id', 'location', 'message_id', 'seen', 'emailed'];
+const tableFields = ['game_id', 'user_id', 'location', 'message_id', 'seen', 'emailed'];
 
-exports.find = async function(conditions){
-    const queryParts = [];
-    const queryData = [];
-    for (const field of tableFields){
-        if (_.has(conditions, field)){
-            queryParts.push(field + ' = $' + (queryParts.length+1));
-            queryData.push(conditions[field]);
+const ReadMessage = new Model('read_messages', tableFields, {
+    keyFields: ['game_id', 'user_id', 'message_id'],
+    order: ['user_id', 'location']
+});
+
+ReadMessage.upsert = async function upsert(data){
+    const self = this;
+    const read_message = await self.findOne({user_id: data.user_id, location: data.location});
+    if (read_message) {
+        for (const field in data){
+            if (_.has(read_message, field)){
+                read_message[field] = data[field];
+            }
         }
+        return self.update(null, read_message);
+    } else {
+        return self.create(data);
     }
-    let query = 'select * from "read_messages"';
-    if (queryParts.length){
-        query += ' where ' + queryParts.join(' and ');
-    }
-    query += ' order by user_id, location';
-    const result = await database.query(query, queryData);
-    return result.rows;
 };
 
-exports.findOne = async function(conditions){
-    const results = await exports.find(conditions);
-    if (results.length){
-        return results[0];
-    }
-    return;
-};
+module.exports = ReadMessage;
 
-exports.create = async function(data){
-    if (! validate(data)){
-        throw new Error('Invalid Data');
-    }
-    const queryFields = [];
-    const queryData = [];
-    const queryValues = [];
-    for (const field of tableFields){
-        if (_.has(data, field)){
-            queryFields.push(field);
-            queryValues.push('$' + queryFields.length);
-            queryData.push(data[field]);
-        }
-    }
-
-    let query = 'insert into "read_messages" (';
-    query += queryFields.join (', ');
-    query += ') values (';
-    query += queryValues.join (', ');
-    query += ')';
-
-    await database.query(query, queryData);
-};
-
-exports.update = async function(data){
-    if (! validate(data)){
-        throw new Error('Invalid Data');
-    }
-    const queryUpdates = [];
-    const queryData = [];
-    for (const field of tableFields){
-        if (_.has(data, field)){
-            queryUpdates.push(field + ' = $' + (queryUpdates.length+1));
-            queryData.push(data[field]);
-        }
-    }
-
-    let query = 'update "read_messages" set ';
-    query += queryUpdates.join(', ');
-    query += ' where user_id = $1 and location = $2';
-
-    await database.query(query, queryData);
-};
 
 exports.upsert = async function(data){
-    const read_message = await exports.findOne({user_id: data.user_id, location: data.location});
+    const read_message = await exports.findOne({game_id: data.game_id, user_id: data.user_id, location: data.location});
     if (read_message) {
         for (const field in data){
             if (_.has(read_message, field)){
